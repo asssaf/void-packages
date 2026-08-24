8c194749410ed68fbf6197be495113259afc614b
assaf <445552+asssaf@users.noreply.github.com>
2026-08-23 23:20:30 -0700

    convert package

---
+++ common/travis/convert-pkg.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eux
+
+DEST_DIR="hostdir/binpkgs"
+
+cat > /tmp/excludes <<EOF
+usr/share/man
+usr/share/doc
+usr/share/info
+var/db/xbps
+EOF
+
+
+function convert_package() {
+	local PKG="$1"
+        local VCE_INSTALLED="/usr/local/vce.installed/${PKG}"
+	local PKG_DIR="$(mktemp -d)"
+	zstdcat ${DEST_DIR}/${PKG}*.xbps | tar xv -C ${PKG_DIR}
+
+	mkdir -p "${PKG_DIR}/${VCE_INSTALLED}"
+	if [ -e "${PKG_DIR}/INSTALL" ]; then mv "${PKG_DIR}/INSTALL" "${PKG_DIR}/${VCE_INSTALLED}/"; fi
+	if [ -e "${PKG_DIR}/REMOVE" ]; then mv "${PKG_DIR}/REMOVE" "${PKG_DIR}/${VCE_INSTALLED}/"; fi
+
+	mksquashfs ${PKG_DIR} "${DEST_DIR}/${PKG}.vcz" -ef /tmp/excludes
+	unsquashfs -l "${DEST_DIR}/${PKG}.vcz" | sed 's|^squashfs-root||;/^$/d' > "${DEST_DIR}/${PKG}.list"
+	xbps-rindex -a ${DEST_DIR}/*.xbps
+	xbps-query -R --repository ${DEST_DIR} -x "${PKG}" | sed 's/\(.*\)[-><].*$/\1/' | sed -n '/musl/!p' | sed -n '/^$/!p' > "${DEST_DIR}/${PKG}.dep"
+	cd ${DEST_DIR} && sha256sum ${PKG}.vcz > ${PKG}.sha256
+
+        rm -r "${PKG_DIR}"
+}
+
+
+PKGS=$(cat /tmp/templates)
+
+for PKG in $PKGS
+do
+	convert_package $PKG
+done
+
+exit 0
